ce3bf12895a496df1517004bcf93fdedb3ffd96e
Jesse Thompson <j13thompson@student.bridgew.edu>
2022-02-16 17:19:52 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Star star = new Star(15);
+star.DrawSprite();
+// end of program
---
+++ Star.cs
@@ -0,0 +1,26 @@
+class Star{
+    private int posX;
+    private string[] starSprite;
+    public Star(int posX) {
+        this.posX = posX;
+        starSprite = new string[6];
+        SetSprite();
+    }
+    private void SetSprite() {
+        starSprite[0] = @"          ";
+        starSprite[1] = @"  ___/\___";
+        starSprite[2] = @"  \  ||  / ";
+        starSprite[3] = @"  /__  __\";
+        starSprite[4] = @"     \/   ";
+        starSprite[5] = @"          "; 
+    }
+    public void DrawSprite() {
+        string spaces = " ";
+        for (int i = 0; i < posX; i++) {
+            spaces += " ";
+        }
+        foreach(string eachLine in starSprite) {
+            Console.WriteLine(spaces + eachLine);
+        }
+    }
+}
